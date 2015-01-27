c14deb174a762ab0db1f3fa830ed043b54c7bdc8
ssc9309 <ssc9309@hotmail.com>
2015-01-27 00:54:40 -0500

    Basic functions
    Number and location of blobs

--- CaptainFuckingAhab.ino
+++ CaptainFuckingAhab.ino
@@ -7,6 +7,17 @@ PVision greenCam;
 
 
 int ledPin = 13;
+byte greenCamResult;
+int numOfBlobs = 0;
+
+//Captain Ahab (rover) will be fired and lowered by Pequod (crane) during deployment phase.
+//He will then s
+boolean deploymentPhaseOver = false;
+boolean scanPhaseOver = false;
+boolean travelPhaseOver = false;
+boolean retrievalPhaseOver = false;
+
+
 
 void setup()
 {
@@ -17,7 +28,74 @@ void setup()
 
 void loop()
 {
+  greenCamResult = greenCam.read();
   
+  //if any blobs are found
+  if (greenCamResult)
+  {
+    numOfBlobs = CountBlobs();
+    DisplayLocationsOfBlobs();
+  }
+}
+
+int CountBlobs()
+{
+  int count = 0;
+  
+  if (BLOB1)
+  {
+    count++;
+  }
+  if (BLOB2)
+  {
+    count++;
+  }
+  if (BLOB3)
+  {
+    count++;
+  }
+  if (BLOB4)
+  {
+    count++;
+  }
+  
+  return count;
+}
+
+void DisplayLocationsOfBlobs()
+{
+  if (BLOB1)
+  {
+    Serial.println("BLOB1");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob1.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob1.Y);
+  }
+  if (BLOB2)
+  {
+    Serial.println("BLOB2");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob2.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob2.Y);
+  }
+  if (BLOB3)
+  {
+    Serial.println("BLOB3");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob3.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob3.Y);
+  }
+  if (BLOB4)
+  {
+    Serial.println("BLOB4");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob4.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob4.Y);
+  }
 }
 
 void TestLEDBlink()
