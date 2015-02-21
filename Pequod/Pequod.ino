090fb8dfb3e72d7d8ab84587eebcdf03a8aeb1a6
ssc9309 <ssc9309@hotmail.com>
2015-02-21 17:07:16 -0500

    Pequod: Receiving test code.
    RF communication test code. This is the receiving end.
    Pequod will read a decimal and display as it comes.

--- Pequod/Pequod.ino
+++ Pequod/Pequod.ino
@@ -1,9 +1,24 @@
-void setup() {
+//TX out of Arduino is Digital Pin 1
+//RX into Arduino is Digital Pin 0
+
+int incomingByte = 0;
+
+
+void setup() 
+{
   // put your setup code here, to run once:
 
+  //pretty sure our model is 4800. If this doesn't work, use 2400
+  Serial.begin(4800);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available() > 0)
+  {
+    incomingByte = Serial.read();
+    Serial.println(incomingByte, DEC);
+  }
+  
+  incomingByte = -1;
 }
