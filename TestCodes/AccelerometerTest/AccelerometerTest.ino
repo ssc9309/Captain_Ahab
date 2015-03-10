59cd6d986dbaf22983bdc23aa5c6da0a7ba3f142
ssc9309 <ssc9309@hotmail.com>
2015-03-10 00:25:15 -0400

    FreeIMU library

---
+++ TestCodes/AccelerometerTest/AccelerometerTest.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <EEPROM.h>
+
+#include <ADXL345.h>
+#include <bma180.h>
+#include <HMC58X3.h>
+#include <ITG3200.h>
+#include <MS561101BA.h>
+#include "I2Cdev.h"
+#include "MPU60X0.h"
+
+#include "DebugUtils.h"
+
+#include "FreeIMU.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ TestCodes/GreenCamTest/GreenCamTest.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <PVision.h>
+
+PVision greenCam;
+
+byte greenCamResult;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("Setup");
+  greenCam.init();
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  greenCamResult = greenCam.read();
+  
+  Serial.print(millis());
+  Serial.print(": greenCamResult: ");
+  Serial.println(greenCamResult);
+  
+  if (greenCam.Blob1.Size > 0)
+  {
+    Serial.println("BLOB1");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob1.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob1.Y);
+  }
+  if (greenCam.Blob2.Size > 0)
+  {
+    Serial.println("BLOB2");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob2.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob2.Y);
+  }
+  if (greenCam.Blob3.Size > 0)
+  {
+    Serial.println("BLOB3");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob3.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob3.Y);
+  }
+  if (greenCam.Blob4.Size > 0)
+  {
+    Serial.println("BLOB4");
+    Serial.print("X: ");
+    Serial.println(greenCam.Blob4.X);
+    Serial.print("Y: ");
+    Serial.println(greenCam.Blob4.Y);
+  }
+  
+  delay(1000);
+}
