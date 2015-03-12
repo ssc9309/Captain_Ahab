4eead54b8de0e983bca975472174685c407f8835
ssc9309 <ssc9309@hotmail.com>
2015-03-12 19:18:14 -0400

    Working on MasterTestCode

--- TestCodes/MasterTestCode/MasterTestCode.ino
+++ TestCodes/MasterTestCode/MasterTestCode.ino
@@ -1,11 +1,50 @@
+//The rover will go straight
+//Until wall is found.
+//Then turn until no wall is seen
+//Also continuously update blob data
+//Stop motor if rover is tilted, and turn servo
+//Talk dirty to Pequod.
+
+#include <Wire.h>
+#include <PVision.h>
+#include <VirtualWire.h>
+#include <ServoTimer2.h>
+#include <NewPing.h>
+#include <pt.h>
+
+//Threading
+//pt1 = motordrive
+static struct pt pt1, pt2, pt3;
+int stepperSpeed = 0; //in HZ up to 1500
+char stepperDirection = 's'; //s = stop, f = forward, r = right, l = left, b = back
 
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
-
+  
+  Serial.begin(9600);
+  
+  
+  ThreadingSetup();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void ThreadingSetup()
+{
+  PT_INIT(&pt1);
+  PT_INIT(&pt2);
+  PT_INIT(&pt3);
+}
 
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  AhabMovementThread(&pt1);
+}
+
+static int AhabMovementThread(struct pt *pt)
+{
+  PT_BEGIN(pt);
+  
+  PT_END(pt);
 }
